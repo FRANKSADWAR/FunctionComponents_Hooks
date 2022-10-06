51fea248fef17f7cb5ae6598e45ff880447ed3d7
FRANKSADWAR <billyfranks98@gmail.com>
2022-10-06 10:27:42 +0300

    functionals

---
+++ functionComponents_1.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+// Friend as a function component
+export const Friend = () =>{
+    return <img src="https://content.codecademy.com/courses/React/react_photo-octopus.jpg"/>;
+}
+
