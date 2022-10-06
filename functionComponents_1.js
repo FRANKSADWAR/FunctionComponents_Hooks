3fc45d707529660308ef1c42fb0d18e9fe5aa951
FRANKSADWAR <billyfranks98@gmail.com>
2022-10-06 12:29:41 +0300

    Hooks

--- functionComponents_1.js
+++ functionComponents_1.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+/**
+ * Function components are React components defined as JS functions
+ * Function components must return JSX
+ * Function components may accept a props parameter
+ * 
+ */
 
 // Friend as a function component
 export const Friend = () =>{
@@ -14,4 +20,18 @@ class Friend_class extends React.Component{
         return <img src="https://content.codecademy.com/courses/React/react_photo-octopus.jpg"/>;
     }
 }
+
+// class based component 
+export class MyComponentClass extends React.Component {
+    render(){
+        return <h1>Class based components</h1>;
+    }
+}
+
+// function based component of the same
+export const MyComponentFunction = () => {
+    return <h1>Function based componet</h1>;
+}
+
+
 ReactDOM.render(<Friend/>, document.getElementById('app'));
