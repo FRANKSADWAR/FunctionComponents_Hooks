6cfb02d8b698763cb26baa57065af1361edbf9b4
FRANKSADWAR <billyfranks98@gmail.com>
2022-10-06 10:30:47 +0300

    added

--- functionComponents_1.js
+++ functionComponents_1.js
@@ -7,3 +7,11 @@ export const Friend = () =>{
     return <img src="https://content.codecademy.com/courses/React/react_photo-octopus.jpg"/>;
 }
 
+
+// friend as a class component
+class Friend_class extends React.Component{
+    render(){
+        return <img src="https://content.codecademy.com/courses/React/react_photo-octopus.jpg"/>;
+    }
+}
+ReactDOM.render(<Friend/>, document.getElementById('app'));
