9a9a6bb41cd9121fe8ab06e7d9a3fc516752533c
FRANKSADWAR <billyfranks98@gmail.com>
2022-10-30 22:55:19 +0300

    everyday

---
+++ Use_Effect/PageTitleClass.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class PageTitle extends React.Component{
+    constructor(props){
+        super(props);
+        this.state ={
+             name: ''
+        };
+        
+    }
+
+    componentDidMount(){
+
+    }
+
+    componentDidUpdate(){
+
+    }
+    render(){
+
+    }
+}
