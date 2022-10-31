97623678a8f9dac1f7082ddc3bcae5432a8065e0
FRANKSADWAR <billyfranks98@gmail.com>
2022-10-31 07:51:50 +0300

    created

--- Use_Effect/PageTitleFunction.js
+++ Use_Effect/PageTitleFunction.js
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom";
 function PageTitle(){
     const [title,setTitle] = useState('');
     
+    // in the function components, we use the useEffect function to update, and set side effects for the components as compared to the three lifecycle methods in class based components
     useEffect(()=>{
         document.title = `Hi ${title}`;
     },[title]);
@@ -13,7 +14,12 @@ function PageTitle(){
             <p>
                 Use {title} input field below to rename this page !
             </p>
+            <input
+                onChange={({target})=> setName(target.value)}
+                value = {title}
+                type ='text'
+            />
         </div>
-    )
+    );
 
 }
