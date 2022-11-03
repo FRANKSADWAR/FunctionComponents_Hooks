c19e0bef56c994b7f5c166abf2661b23557ed534
FRANKSADWAR <billyfranks98@gmail.com>
2022-11-03 20:03:53 +0300

    created pull

--- Use_Effect/CleanUp.js
+++ Use_Effect/CleanUp.js
@@ -3,16 +3,28 @@ import ReactDOM from "react-dom";
 
 function Counter(){
 
+    const [clickCount, setClickCount ] = useState(0);
+    
+    // use effects to update components after mounting them 
     useEffect(() =>{
         document.addEventListener('mousedown',increment);
-        return () => {
+
+        return () => { // clean up after the effects
             document.removeEventListener('mousedown',increment);
         }
     },[]);
-}
 
-const increment = () => {
-    setClick((prevCount) => prevCount + 1);
-};
+    // set the current state using the previous state
+    const increment = () => {
+        setClickCount((prevCount) => prevCount + 1);
+    };
+
+    return (
+        <h1>Document clicks: {clickCount} </h1>
+    );
+}
+ReactDOM.render(<Counter/>,document.getElementById('counter'));
+
+
 
 
